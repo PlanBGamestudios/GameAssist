1bc50b55126a9af23f17594fccb768ba1c2c432a
Matthias Wurm <wurm.matthias@gmx.net>
2019-06-03 19:43:04 +0200

    statusBar fix

--- GameAssist/src/app/app.component.ts
+++ GameAssist/src/app/app.component.ts
@@ -20,6 +20,9 @@ export class AppComponent {
   initializeApp() {
     this.platform.ready().then(() => {
       this.statusBar.styleDefault();
+      // let status bar overlay webview
+      this.statusBar.overlaysWebView(true);
+      this.statusBar.backgroundColorByHexString('#33000000');
       this.splashScreen.hide();
     });
   }
