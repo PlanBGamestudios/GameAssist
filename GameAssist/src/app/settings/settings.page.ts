136883cf1618a1a59d9a2dd394f47be58438e519
Matthias Wurm <wurm.matthias@gmx.net>
2019-06-03 07:17:03 +0200

    Toast and Checkbox test

--- GameAssist/src/app/settings/settings.page.ts
+++ GameAssist/src/app/settings/settings.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { ToastController } from '@ionic/angular';
 
 @Component({
   selector: 'app-settings',
@@ -7,6 +8,20 @@ import { Component } from '@angular/core';
 })
 export class settingsPage {
 
-  constructor() {}
+  constructor(public toastController: ToastController) {}
 
+    async presentToast() {
+      const toast = await this.toastController.create({
+        message: 'Your settings have been saved.',
+        duration: 2000
+      });
+      toast.present();
+    }
+
+    public form = [
+    { val: 'Awesome setting #1', isChecked: true },
+    { val: 'Enable statistics', isChecked: true },
+    { val: 'Awesome setting #2', isChecked: false },
+    { val: 'Awesome setting #3', isChecked: true }
+  ];
 }
